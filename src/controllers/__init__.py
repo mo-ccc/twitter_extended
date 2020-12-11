7958de84a6eb036af909552959916d2aa637de1f
Mo <css012002@coderacademy.edu.au>
2020-12-11 20:39:40 +1100

    added tweet post

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,5 +1,6 @@
 import flask
 from controllers.user_controller import users
 from controllers.auth_controller import auth
+from controllers.tweet_controller import tweets
 
-blueprints = [users, auth]
+blueprints = [users, auth, tweets]
--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,12 +1,17 @@
 import flask
 from models.User import User
+from models.Tweet import Tweet
+from schemas.UserSchema import UserSchema
+from schemas.TweetSchema import TweetSchema
 
 users = flask.Blueprint("users", __name__)
 
-@users.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    print(users)
-    for x in users:
-        print(x.name)
-    return 'ok'
+@users.route('/users/<id>', methods=['GET'])
+def get_user(id):
+    user = User.query.filter_by(id=id).first_or_404()
+    user_dump = UserSchema().dump(user)
+    
+    tweets = Tweet.query.filter_by(author_id=id).all()
+    tweets_dump = TweetSchema(many=True).dump(tweets)
+    
+    return flask.jsonify({"author": user_dump, "tweets": tweets_dump})
